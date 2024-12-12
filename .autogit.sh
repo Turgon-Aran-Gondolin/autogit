b26db1245dedc36065caf2e80f6110474c8eb8da
Yingsheng Huang <Turgon-Aran-Gondolin@outlook.com>
2024-12-12 16:13:37 -0700

    Update .autogit.sh

--- .autogit.sh
+++ .autogit.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# - Copy all the files in the repository to the root directory of the target repository. 
+# - First change the branch and upstream names in the subroutine file, then add the contents in the .gitignore example to the actual .gitignore file. 
+# - Create the backup branch as what's defined in the script. 
+# - Config vscode tasks to automatically run the script. 
+# - Default backup interval is one day (after initial execution). 
+
 while true
 do
     echo 'backup start'
