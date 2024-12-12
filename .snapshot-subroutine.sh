c818fe1dce6c15258d9b43198bd1179959aa492b
Yingsheng Huang <Turgon-Aran-Gondolin@outlook.com>
2024-12-12 16:04:56 -0700

    Update .snapshot-subroutine.sh

--- .snapshot-subroutine.sh
+++ .snapshot-subroutine.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 BRANCH=backup
+UPSTREAM=origin
+DATE=$(date +%F)
 export GIT_INDEX_FILE=/tmp/git-backup-index.$$
 git add .
-git commit-tree $(git write-tree) -p $(git show-ref --hash --heads $BRANCH) -m "Automatic backup" | xargs git branch -f $BRANCH
+git commit-tree $(git write-tree) -p $(git show-ref --hash --heads $BRANCH) -m "Automatic backup ${DATE}" | xargs git branch -f $BRANCH
 rm -f "$GIT_INDEX_FILE"
+git push $UPSTREAM $BRANCH
