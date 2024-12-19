03aa4a72f0205237a8661d372bd5cd46008b999c
Yingsheng Huang <Turgon-Aran-Gondolin@outlook.com>
2024-12-19 03:03:10 -0700

    Update load-autogit.sh

--- load-autogit.sh
+++ load-autogit.sh
@@ -7,5 +7,6 @@ unzip master.zip
 rm master.zip
 mv autogit-master/.* ./
 cat .gitignore.example >> .gitignore
-cat tasks.json.example >> .vscode/tasks.json
+# cat tasks.json.example >> .vscode/tasks.json
+jq -s '.[0] * {"tasks": (.[0].tasks + .[1].tasks | unique_by(.label))}' tasks.example.json .vscode/tasks.json >! .vscode/tasks.json
 git branch $BACKUP master
