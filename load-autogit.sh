06e9a07b50736feaaf042490024741e9bde43cc6
Yingsheng Huang <Turgon-Aran-Gondolin@outlook.com>
2024-12-20 01:38:48 -0700

    Update load-autogit.sh

--- load-autogit.sh
+++ load-autogit.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/bash
 
 if [ -z "$1" ]; then
-  $BACKUP=backup
+  BACKUP=backup
 else
-  $BACKUP=$1
+  BACKUP=$1
 fi
+# echo $BACKUP
 
 wget https://github.com/Turgon-Aran-Gondolin/autogit/archive/refs/heads/master.zip
 unzip master.zip
 rm master.zip
+rm autogit-master/load-autogit.sh
 mv autogit-master/* ./
+mv autogit-master/.* ./
 rm autogit-master -r
 
 sed -i "s/^BRANCH=.*$/BRANCH=$BACKUP/" .snapshot-subroutine.sh
@@ -17,5 +20,5 @@ git branch $BACKUP master
 
 cat .gitignore.example >> .gitignore
 # cat tasks.json.example >> .vscode/tasks.json
-jq -s '.[0] * {"tasks": (.[0].tasks + .[1].tasks | unique_by(.label))}' tasks.example.json .vscode/tasks.json > .vscode/tasks.json
+jq -s 'map(select(.tasks != null) | .tasks) | add | unique_by(.label) | {version: "2.0.0", tasks: .}' tasks.example.json .vscode/tasks.json > .vscode/tasks.json
 
